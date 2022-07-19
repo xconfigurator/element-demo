0744c29c4190ae80815ae8e293fa12924219b8e9
liuyang <xconfigurator@hotmail.com>
2022-07-19 16:05:06 +0800

    Add Form demo

--- src/views/TestInput.vue
+++ src/views/TestInput.vue
@@ -2,11 +2,24 @@
     <div>
         <el-link href="https://element.eleme.cn/#/zh-CN/component/input" target="_blank" type="primary">文档 Input</el-link>
         <br>
-        <!-- prefix-icon suffix-icon -->
+        <!-- 控制高度 size -->
+        <!-- 图标 prefix-icon suffix-icon -->
         <!-- 限制宽度 -->
-        <div style="width: 200px;">
-            <el-input placeholder="演示输入框两端的小图标" prefix-icon="el-icon-phone" suffix-icon="el-icon-delete-solid"></el-input>
+        <div style="width: 800px;">
+            <el-input placeholder="演示输入框两端的小图标" v-model="intput1" prefix-icon="el-icon-phone" suffix-icon="el-icon-delete-solid"></el-input>
+            <el-input placeholder="展示/隐藏密码" v-model="input2" show-password></el-input>
         </div>
-        <!-- maxlength minlength show-word-limit -->
+        <!-- 限制输入框可输入的字符长度 maxlength minlength show-word-limit (e.g. 0/3) -->
     </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      input1: '',
+      input2: ''
+    }
+  }
+}
+</script>
